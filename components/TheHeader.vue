24bf54131dccfac6568b6a46d856950d50a2bc62
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-03-02 04:36:09 +0100

    home + animation

---
+++ components/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+      <h1>Hugo Tran</h1>
+      <nav>
+          <ul>
+              <li>Accueil</li>
+              <li>Technologies</li>
+              <li>Projets</li>
+              <li>Contact</li>
+          </ul>
+      </nav>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss">
+header{
+    display: flex;
+    justify-content: space-evenly;
+    width:100%;
+    max-height: 5rem;
+    position: fixed;
+    top: 0;
+    left: 0;
+    color: white;
+    z-index: 10;
+    h1{
+        margin: 1rem 0 0 0;
+    }
+    nav{
+        width: 30rem;
+        ul{
+            display: flex;
+            justify-content: space-between;
+            margin: 1rem 1rem 0 0;
+            height: 4rem;
+            align-items: center;
+            list-style-type: none;
+            font-size: 1.5rem;
+        }
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,93 @@
 <template>
   <div>
-    <div class="card test" style="width: 18rem;">
-      <img src="http://www.logic-sunrise.com/images/validation/in-1904976125.png" class="card-img-top" alt="...">
-      <div class="card-body">
-        <h5 class="card-title red">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-      </div>
-    </div>
+    <TheHeader />
+    <main>
+        <div class="Home">
+          <div class="wrapper">
+            <h1>Je suis </h1>
+            <h1 class="typing">hi</h1>
+          </div>
+        </div>
+    </main>
   </div>
 </template>
 
 <script>
 export default {
   name: 'IndexPage',
+  data(){
+    return{
+      texts:["Hugo Tran","Développeur Web",],
+      count:0,
+      index:0,
+      currentText:'',
+      letter:'',
+    }
+  },
+  mounted(){
+    this.type();
+  },
+  methods:{
+    type(){
+      if(this.count===this.texts.length){
+        this.count = 0;
+      }
+      this.currentText = this.texts[this.count];
+      this.letter = this.currentText.slice(0,++this.index)
+      document.querySelector('.typing').textContent = this.letter;
+      
+      if(this.letter.length === this.currentText.length){
+        this.count++;
+        this.index = 0;
+      }
+      setTimeout(this.type, 300);
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 @import '~/assets/scss/_variables.scss';
-.card{
-  .red {
-    color:$red;
+.Home{
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  color: white;
+  background: linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url('~/assets/images/banner.jpg');
+  background-size: cover;
+}
+.wrapper{
+  display: flex;
+  position: relative;
+  left: 38%;
+  h1{
+    font-size: 3rem;
   }
 }
+.typing{
+  position: relative;
+  margin-left: 1rem;
+  font-weight: bold;
+
+  &::after{
+    content:"";
+    position:absolute;
+    right: -0.5rem;
+    width: 1px;
+    height: 100%;
+    border-right: 2px solid white;
+    animation: blink 0.8s infinite ease;
+  }
+}
+@keyframes blink{
+  0%{
+    opacity: 0;
+  }
+  100%{
+    opacity: 1;
+  }
+}
+
+
 
 </style>
