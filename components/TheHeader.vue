8e4c0fc24ea287fd8b0f4bc99332f81505cf7385
Tran-Hugo <82827738+Tran-Hugo@users.noreply.github.com>
2022-03-08 03:48:46 +0100

    Update TheHeader.vue

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -35,7 +35,7 @@ header{
     display: flex;
     justify-content: space-evenly;
     width:100%;
-    max-height: 8vh;
+    height: 8vh;
     position: fixed;
     top: 0;
     left: 0;
@@ -147,7 +147,6 @@ ul a{
       height: fit-content;
       opacity: 1;
       visibility: visible;
-    
   }
   .scrolled{
       animation: unset;
